ace154e0e5b9de8d36fdf110c195e49a5d129871
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-11-08 13:40:01 +0300

    Authentication

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 const RootPage = () => {
-  return <div>Root Page ( protected )</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default RootPage;
