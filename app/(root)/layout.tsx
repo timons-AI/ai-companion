48e8b5401df80ee3caa320fd16cf2872ef97bd01
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-11-08 14:33:13 +0300

    Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,13 @@
 import { Navbar } from "@/components/Navbar";
+import { Sidebar } from "@/components/sidebar";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className=" h-full">
       <Navbar />
+      <div className=" hidden md:flex mt-16 flex-col w-20 fixed inset-y-0">
+        <Sidebar />
+      </div>
       <main className=" md:pl-20 pt-16 h-full">{children}</main>
     </div>
   );
